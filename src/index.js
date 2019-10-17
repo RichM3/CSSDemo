afa6c3adf994100b2d204aa3fcdebae16d44d1c3
RichM3 <richmeyerjobs@gmail.com>
2019-10-16 23:22:27 -0400

    queen rotating, working on revolving cog

--- src/index.js
+++ src/index.js
@@ -58,6 +58,26 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
+        var looper;
+        var degrees = 0;
+
+        function rotateAnimation(el, speed) {
+            var elem = document.getElementById(el);
+            elem.style.transform = "rotate(" + degrees + "deg)";
+
+            looper = setTimeout('rotateAnimation(\'' + el + '\',' + speed + ')', speed);
+
+            degrees++;
+
+            if (degrees > 359) {
+                degrees = 1;
+            }
+
+            document.getElementById("status").innerHTML = "rotate(" + degrees + "deg)";
+        }
+
+
+        // rotateAnimation("img1", 30);
 });
 
 // Document.querySelectorAll()
